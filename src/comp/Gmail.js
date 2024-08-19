999953eb503e67967aa5272545c73f6647c1970d
mnomnanmnasir <nomannasir@i8is.com>
2024-08-19 05:41:15 -0700

    udpated

--- src/comp/Gmail.js
+++ src/comp/Gmail.js
@@ -31,11 +31,11 @@ function Gmail() {
     const email = searchParams.get('email');
     const password = searchParams.get('password');
 
-    useEffect(() => {
-        localStorage.setItem("auth_token", params.token)
-        localStorage.setItem("email", email)
-        localStorage.setItem("password", password)
-    }, [])
+    // useEffect(() => {
+    //     localStorage.setItem("auth_token", params.token)
+    //     localStorage.setItem("email", email)
+    //     localStorage.setItem("password", password)
+    // }, [])
 
     const [items, setItems] = useState([
         { path: 'Home', isActive: false },
