74977a948b78c5f42a9ce41c1400fa502c819434
Abdullah Anjum i8is <abdullahanjum@i8is.com>
2024-09-11 17:34:17 +0500

    agaion and again

--- src/comp/SS-track.js
+++ src/comp/SS-track.js
@@ -7,8 +7,8 @@ function SSTrack() {
     const password = localStorage.getItem("password")
     return (
         <div style={{ padding: '7px 0 0 65px' }}>
-            <iframe src={`https://sstrack-frontinfiniti.vercel.app/dashboard?email=${email}&password=${password}`} style={{ width: "100%", height: "100vh", overflow: "hidden", border: "none" }} />
-        </div>
+        <iframe src={`https://sstrack-frontinfiniti.vercel.app/dashboard?email=${email}&password=${password}`} style={{ width: "100%", height: "100vh", overflow: "hidden", border: "none" }} />;
+    </div>
     )
 }
 
