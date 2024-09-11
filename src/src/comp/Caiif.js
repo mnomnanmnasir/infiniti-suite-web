573b0dccc2aa33fc2b51c832f85cb40e36519c91
Abdullah Anjum i8is <abdullahanjum@i8is.com>
2024-09-11 15:26:50 +0500

    padding

--- src/src/comp/Caiif.js
+++ src/src/comp/Caiif.js
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom';
 
 function Caiif() {
     return (
-        <div style={{ padding: '70px 0 0 65px' }}>
+        <div style={{ padding: '7px 0 0 65px' }}>
             <iframe src={`https://caiif.ca/dashboard`} style={{ width: "100%", height: "100vh", overflow: "hidden !important", border: "none" }} />;
         </div>
     )
